aa457460c94779208be7eba7aed5a3cbd62944ce
joanegarimbao-ux <joane.garimbao@gmail.com>
2025-12-23 13:39:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
 import React from "react";
-import netlifyIdentity from "@netlify/identity-widget";
-
-netlifyIdentity.init();
 
 export default function App() {
-  function login() {
-    netlifyIdentity.open();
-  }
-
-  function logout() {
-    netlifyIdentity.logout();
-  }
-
-  const user = netlifyIdentity.currentUser();
-
   return (
     <div style={{ padding: 20, fontFamily: "Arial" }}>
       <h2>SSS Dagupan DTR</h2>
-
-      {!user && (
-        <button onClick={login} style={{ padding: 10 }}>
-          Log in
-        </button>
-      )}
-
-      {user && (
-        <>
-          <p>Welcome, {user.email}</p>
-          <button onClick={logout} style={{ padding: 10 }}>
-            Log out
-          </button>
-        </>
-      )}
+      <p>Application deployed successfully.</p>
+      <p>Login will be enabled after deployment.</p>
     </div>
   );
 }
